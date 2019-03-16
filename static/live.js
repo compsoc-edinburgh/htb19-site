37a64de25115863b76e837c27a6f4ed5dd73afff
Patrick Kage <quadnix1@gmail.com>
2019-03-16 03:52:03 +0000

    Add SPA paging to live mode, show more information

--- static/live.js
+++ static/live.js
@@ -5,62 +5,65 @@ const timeDifference = date => (date.isAfter(moment())) ? date.fromNow() : date.
 
 const notifiedEvents = {}
 
-const update = () => document.querySelectorAll('.live__scheduleitem').forEach(node => {
-    const date = extractDate(node)
+const update = () => {
+    document.querySelectorAll('.live__scheduleitem').forEach(node => {
+        const date = extractDate(node)
 
-    // force reset the styling
-    node.className = 'live__scheduleitem'
+        // force reset the styling
+        node.className = 'live__scheduleitem'
 
-    // set the coundown
-    node.querySelector('.live__scheduleitem__title--countdown').innerText = `${date.format('dddd hh:mma')} (${timeDifference(date)})`
+        // set the coundown
+        node.querySelector('.live__scheduleitem__title--countdown').innerText = `${date.format('dddd hh:mma')} (${timeDifference(date)})`
 
 
-    // if the date 
-    if (date.isAfter(moment())) {
-        // check if the date is less than ten minutes away
-        let datecopy = moment(date)
-        datecopy = datecopy.subtract(10, 'minutes')
+        // if the date 
+        if (date.isAfter(moment())) {
+            // check if the date is less than ten minutes away
+            let datecopy = moment(date)
+            datecopy = datecopy.subtract(10, 'minutes')
 
-        // if it is, check we haven't raised a notification before
-        if (datecopy.isBefore(moment())) {
-            if (!(node.dataset.time in notifiedEvents)) {
-                // first time
-                notifiedEvents[node.dataset.time] = true
-                
-                let notification = new Notification(`${node.dataset.title} ${date.fromNow()}!`, {
-                    badge: '/static/favicon.png',
-                    icon: '/static/favicon.png'
+            // if it is, check we haven't raised a notification before
+            if (datecopy.isBefore(moment())) {
+                if (!(node.dataset.time in notifiedEvents)) {
+                    // first time
+                    notifiedEvents[node.dataset.time] = true
+                    
+                    let notification = new Notification(`${node.dataset.title} ${date.fromNow()}!`, {
+                        badge: '/static/favicon.png',
+                        icon: '/static/favicon.png'
+                    })
+
+                }
+            }
+
+        } else {
+            // check if the full duration is over if the event start is in the past.
+            let datecopy = moment(date)
+            if (node.dataset.duration !== '') {
+                datecopy = datecopy.add(0 + node.dataset.duration, 'minutes')
+            } else {
+                datecopy = datecopy.add(60, 'minutes')
+            }
+
+            if (datecopy.isAfter(moment())) {
+                // currently happening
+                node.classList.add('live__scheduleitem--active')
+                window.scrollTo({
+                    'behavior': 'smooth',
+                    'left': 0,
+                    'top': node.offsetTop - 100
                 })
 
+            } else {
+                node.classList.add('live__scheduleitem--passed')
             }
         }
-
-    } else {
-        // check if the full duration is over if the event start is in the past.
-        let datecopy = moment(date)
-        if (node.dataset.duration !== '') {
-            datecopy = datecopy.add(0 + node.dataset.duration, 'minutes')
-        } else {
-            datecopy = datecopy.add(60, 'minutes')
-        }
-
-        if (datecopy.isAfter(moment())) {
-            // currently happening
-            node.classList.add('live__scheduleitem--active')
-            window.scrollTo({
-                'behavior': 'smooth',
-                'left': 0,
-                'top': node.offsetTop - 100
-            })
-
-        } else {
-            node.classList.add('live__scheduleitem--passed')
-        }
-    }
-})
+    })
+    
+    // TODO: scroll if there are no active schedule items
+}
 
 window.onload = () => {
-    console.log('window loaded')
     Notification.requestPermission()
     update()
 
@@ -69,3 +72,21 @@ window.onload = () => {
 }
 
 
+// super simple nav
+document.querySelectorAll('[name="livenav"]').forEach(node => {
+    node.addEventListener('change', ev => {
+        const n = ev.target
+        // if we're not checked, stop
+        if (!n.checked) return;
+
+        // otherwise update page state
+        document.querySelectorAll('[data-nav]').forEach(section => {
+            if (section.dataset.nav === n.value) {
+                section.classList.remove('live--hidden')
+            } else {
+                section.classList.add('live--hidden')
+            }
+        })
+    })
+
+})
