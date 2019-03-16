297b6c97bb0b1dba9dae6de04eddac7af0170492
Patrick Kage <quadnix1@gmail.com>
2019-03-16 01:44:47 +0000

    Add preliminary live mode

---
+++ static/live.js
@@ -0,0 +1,71 @@
+const extractDate = node => moment(node.dataset.time)
+
+// according to Moment.js docs, fromNow will not overlap with toNow. This is not true.
+const timeDifference = date => (date.isAfter(moment())) ? date.fromNow() : date.fromNow()
+
+const notifiedEvents = {}
+
+const update = () => document.querySelectorAll('.live__scheduleitem').forEach(node => {
+    const date = extractDate(node)
+
+    // force reset the styling
+    node.className = 'live__scheduleitem'
+
+    // set the coundown
+    node.querySelector('.live__scheduleitem__title--countdown').innerText = `${date.format('dddd hh:mma')} (${timeDifference(date)})`
+
+
+    // if the date 
+    if (date.isAfter(moment())) {
+        // check if the date is less than ten minutes away
+        let datecopy = moment(date)
+        datecopy = datecopy.subtract(10, 'minutes')
+
+        // if it is, check we haven't raised a notification before
+        if (datecopy.isBefore(moment())) {
+            if (!(node.dataset.time in notifiedEvents)) {
+                // first time
+                notifiedEvents[node.dataset.time] = true
+                
+                let notification = new Notification(`${node.dataset.title} ${date.fromNow()}!`, {
+                    badge: '/static/favicon.png',
+                    icon: '/static/favicon.png'
+                })
+
+            }
+        }
+
+    } else {
+        // check if the full duration is over if the event start is in the past.
+        let datecopy = moment(date)
+        if (node.dataset.duration !== '') {
+            datecopy = datecopy.add(0 + node.dataset.duration, 'minutes')
+        } else {
+            datecopy = datecopy.add(60, 'minutes')
+        }
+
+        if (datecopy.isAfter(moment())) {
+            // currently happening
+            node.classList.add('live__scheduleitem--active')
+            window.scrollTo({
+                'behavior': 'smooth',
+                'left': 0,
+                'top': node.offsetTop - 100
+            })
+
+        } else {
+            node.classList.add('live__scheduleitem--passed')
+        }
+    }
+})
+
+window.onload = () => {
+    console.log('window loaded')
+    Notification.requestPermission()
+    update()
+
+    // update every 5 minutes
+    setInterval(update, 5 * 60 * 1000)
+}
+
+
